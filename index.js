3355550c26794bdb1862af1f2c1027d6d637b0e9
guynitt11-code <guynitt11@gmail.com>
2026-02-01 16:57:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -31,4 +31,8 @@ app.post("/render", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
